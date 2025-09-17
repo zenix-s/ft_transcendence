d0556e5231a59c2ba19eda95e97b1439a0619d2a
Ines Sainz Ramon <isainz-r@c3r8s3.42madrid.com>
2025-09-17 12:24:25 +0200

    make clases to store information, make a function to actualize the html, correct small mestake in the html and change ids

--- frontend/src/modules/game/game.ts
+++ frontend/src/modules/game/game.ts
@@ -12,31 +12,110 @@ document.addEventListener("keydown", (event) => {
 		console.log("s");
 });
 
-// interface Player {
-// 	paddle: HTMLElement,
-// 	position: number,
-// 	colisions: number,
-// }
+interface Player {
+	paddle: HTMLElement,
+	posX: number,
+	posY: number,
+	height: number,
+	width: number,
+	speed: number,
+	topPercentage: number,
+	bottomPercentage: number,
+}
 
-// class Player {
-// 	paddle: HTMLElement;
-// 	position: number;
-// 	colisions: number;
-// 	constructor(paddle : HTMLElement) {
-// 		this.paddle = paddle;
-// 		this.position = 0;
-// 		this.colisions = 100;
-// 	}
-// }
+class Player{
+	paddle: HTMLElement;
+	posX: number;
+	posY: number;
+	height: number;
+	width: number;
+	speed: number;
+	topPercentage: number;
+	bottomPercentage: number;
 
-/* top: 5.6 bottom: 94.5 */
-function loopGame(canvas:HTMLCanvasElement, paddle1:HTMLElement, paddle2:HTMLElement)
+	constructor(padddle: HTMLElement, posX: number){
+		this.paddle = padddle;
+		this.posX = posX;
+		this.posY = 625;
+		this.height = 32;
+		this.width = 4;
+		this.speed = 1;
+		this.topPercentage = 5.8;
+		this.bottomPercentage = 94.2;
+	}
+}
+
+interface Score {
+	scoreLeft: HTMLElement | null,
+	pointsLeft: number,
+	scoreRight: HTMLElement | null,
+	pointsRight: number,
+	maxScore: number,
+}
+
+class Score {
+	scoreLeft: HTMLElement | null;
+	pointsLeft: number;
+	scoreRight: HTMLElement | null;
+	pointsRight: number;
+	maxScore: number;
+
+	constructor(maxScore: number) {
+		this.scoreLeft = document.getElementById("scoreLeft");
+		this.pointsLeft = 0;
+		this.scoreRight = document.getElementById("scoreRight");
+		this.pointsRight = 0;
+		this.maxScore = maxScore;
+	}
+}
+
+interface Ball {
+	posX: number,
+	posY: number,
+	speed: number,
+}
+
+class Ball {
+	posX: number;
+	posY: number;
+	speed: number;
+
+	constructor() {
+		this.posX = 1000;
+		this.posY = 625;
+		this.speed = 1;
+	}
+}
+
+function loopGame(canvas:HTMLCanvasElement, playerLeft: Player, playerRight: Player,
+	scores: Score , ball: Ball)
 {
+	console.log(canvas.height);
 	if (canvas.height < 500)
 	{
-		paddle1.classList.replace("h-32", "h-10");
-		paddle2.classList.replace("h-32", "h-10");
+		playerLeft.paddle.classList.replace("h-32", "h-10");
+		playerLeft.paddle.classList.replace("h-32", "h-10");
 	}
+
+	/* CAMBIAR JUGADORES DE SITIO */
+	playerLeft.paddle.style.top = "5.8%"; //"500px";
+	playerRight.paddle.style.top = "94.2%"; //"1000px";
+
+	/* CAMBIAR LA PELOTA DE SITIO */
+	const ctx = canvas.getContext("2d")!;
+	ctx.fillStyle = "black";
+	ctx.beginPath();
+	ctx.arc(2000/2, 1250/2, 15, 0, Math.PI * 2);
+	ctx.fill();
+	ctx.fillStyle = "white";
+	ctx.beginPath();
+	ctx.arc(200, 200, 15, 0, Math.PI * 2);
+	ctx.fill();
+
+	/* CAMBIAR PUNTUACIONES */
+	scores.pointsLeft = 10;
+	scores.scoreLeft.textContent = scores.pointsLeft.toString();
+	scores.scoreRight.textContent = scores.pointsRight.toString();
 }
 
 export async function startGame()
@@ -51,49 +130,43 @@ export async function startGame()
 	}
 	
 	/* JUGADORES */
-	const paddle1 = document.getElementById("player1") as HTMLElement;
-	const paddle2 = document.getElementById("player2") as HTMLElement;
+	const paddle1 = document.getElementById("playerLeft") as HTMLElement;
+	const paddle2 = document.getElementById("playerRight") as HTMLElement;
+	if (!paddle1 || !paddle2)
+	{
+		console.log("Error: no paddles");
+		return ;
+	}
 
-	console.log(paddle1);
-	console.log(paddle2);
+	const playerLeft = new Player(paddle1, 10);
+	const playerRight = new Player(paddle2, 1990);
+	console.log(playerLeft);
+	console.log(playerRight);
+
 	console.log("hola");
 
 	paddle1.style.top = "50%"; //"500px";
 	paddle2.style.top = "50%"; //"1000px";
 
-	console.log(paddle1);
-	console.log(paddle2);
-
-	// const player1 = new Player(paddle1);
-	// const player2 = new Player(paddle2);
-	// console.log(player1);
-	// console.log(player2);
-
-
-	/* PUNTUACIONES */
-	if (!document.getElementById("score1")?.innerHTML)
+	/* SCORE */
+	const scores = new Score(5);
+	if (!scores.scoreLeft || !scores.scoreRight)
 	{
+		console.log("Error: no scores");
 		return ;
 	}
-	let score1 = document.getElementById("score1").innerHTML = "10";
-	console.log("score1=", score1);
-	score1 = "10";
+	console.log("Scores=", scores);
 
-
-	/* PELOTA */
-	const cssW = 2000;
-	const cssH = 1250;
-	
-	// Center of the canvas (CSS pixels)
-	let posX = cssW / 2;
-	let posY = cssH / 2;
+	/* PELOTA GOOD */
+	const ball = new Ball();
+	console.log("ball=", ball);
 
 	console.log("patataassss");
 	
 	// Draw a white dot in the center
 	ctx.fillStyle = "white";
 	ctx.beginPath();
-	ctx.arc(posX, posY, 15, 0, Math.PI * 2);
+	ctx.arc(ball.posX, ball.posY, 15, 0, Math.PI * 2);
 	ctx.fill();
-	loopGame(canvas, paddle1, paddle2);
+	loopGame(canvas, playerLeft, playerRight, scores, ball);
 }

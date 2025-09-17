216c0b97250714ce45655f596aeafa854c70e1ce
BishopVK <danielgraindorge@gmail.com>
2025-09-17 22:59:06 +0200

    Fixing responsive breakpoints. Stylizing dashboard

--- frontend/src/components/GlitchButton.ts
+++ frontend/src/components/GlitchButton.ts
@@ -7,7 +7,7 @@ export function GlitchButton(
 	button.textContent = text;
 	button.className = `
 	  relative bg-transparent text-secondary border border-secondary 
-	  px-8 py-4 font-mono text-5xl skew-x-[5deg]
+	  px-8 py-4 font-mono text-3xl sm:text-5xl skew-x-[5deg]
 	  cursor-pointer transition-all duration-1000 shadow-none 
 	  hover:shadow-[5px_0_5px_red,-5px_0_5px_blue] 
 	  hover:text-shadow-[5px_0_0_red,-5px_0_0_blue] 

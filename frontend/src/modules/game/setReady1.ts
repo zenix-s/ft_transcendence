9a1df32bdccfad99d7ec75908509d30292346bb4
Ines Sainz Ramon <isainz-r@c2r10s2.42madrid.com>
2025-10-08 15:38:51 +0200

    change letters size and add selected options for setReady1 default page

---
+++ frontend/src/modules/game/setReady1.ts
@@ -0,0 +1,10 @@
+document.addEventListener("click", () => {
+	const difficulty = document.getElementById("difficultyAI");
+	const maxPoints = document.getElementById("puntosMax");
+	if (!difficulty || !maxPoints)
+	{
+		console.log("problemas");
+		return ;
+	}
+	
+})

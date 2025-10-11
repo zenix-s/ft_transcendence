dab0b733e4053dda9a4b0bebba363005656f10c1
zenix-s <sergioffdev@gmail.com>
2025-10-11 23:10:16 +0200

    Update git merge conflict

--- backend/src/features/user-manager/UserManager.presentation.ts
+++ backend/src/features/user-manager/UserManager.presentation.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from 'fastify';
 import UpdatePasswordRoute from './updatePassword/updatePassword.route';
 import UploadAvatarRoute from './UploadAvatar/UploadAvatar.route';
-import UpdateUsernameRoute from './updateUsername/updateUsername.route';
+import UpdateUsernameRoute from './UpdateUsername/UpdateUsername.route';
 
 export default function userManagerRoutes(fastify: FastifyInstance) {
     fastify.register(UploadAvatarRoute);

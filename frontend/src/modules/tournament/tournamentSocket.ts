b8c54f76681da92f418b0bdce77c8ff384fe384f
BishopVK <danielgraindorge@gmail.com>
2025-11-27 22:33:40 +0100

    Fixed backend prefix to tournament websocket. Fixed path to connect ws. Fixed vite proxy to tournaments ws

--- frontend/src/modules/tournament/tournamentSocket.ts
+++ frontend/src/modules/tournament/tournamentSocket.ts
@@ -102,7 +102,7 @@ export class TournamentWebSocketClient {
   private tournaments: Tournament[] = [];
 
   constructor(token: string) {
-    this.wsUrl = getWsUrl("/ws/tournaments/pong"); // Actualizar al EndPoint correspondiente a los torneos
+    this.wsUrl = getWsUrl("/tournaments/pong"); // Actualizar al EndPoint correspondiente a los torneos
     this.token = token;
   }
 
